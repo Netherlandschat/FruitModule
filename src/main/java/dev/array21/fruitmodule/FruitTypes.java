cc2dab8df7f12209f14b6076754abf9d44edd458
Daniël Vos <danielvos@outlook.com>
2021-06-28 23:09:08 +0200

    Add more fruits etc.

--- src/main/java/dev/array21/fruitmodule/FruitTypes.java
+++ src/main/java/dev/array21/fruitmodule/FruitTypes.java
@@ -10,8 +10,8 @@ public class FruitTypes {
             fruitMap.put("bananen", "U+1F34C");
             fruitMap.put("banana", "U+1F34C");
 
-            fruitMap.put(" appel", "U+1F34E");
-            fruitMap.put(" apple", "U+1F34E");
+            fruitMap.put(" appel", "U+1F34F");
+            fruitMap.put(" apple", "U+1F34F");
 
             fruitMap.put("peer", "U+1F350");
             fruitMap.put("pear", "U+1F350");
@@ -19,7 +19,15 @@ public class FruitTypes {
             fruitMap.put("aardbei", "U+1F353");
             fruitMap.put("strawberr", "U+1F353");
 
+            fruitMap.put("sinaasappel", "U+1F34A");
+            fruitMap.put("orange", "U+1F34A");
+
+            fruitMap.put("kiwi", "U+1F95D");
+
             fruitMap.put("mango", "U+1F96D");
+
+            fruitMap.put("ananas", "U+1F34D");
+            fruitMap.put("pineapple", "U+1F34D");
 
             fruitMap.put("tomaat", "U+1F345");
             fruitMap.put("tomato", "U+1F345");
@@ -28,10 +36,34 @@ public class FruitTypes {
             fruitMap.put("watermelon", "U+1F349");
 
             fruitMap.put("broccoli", "U+1F966");
+            fruitMap.put("tinus", "U+1F451");
+
+            fruitMap.put("potato", "U+1F954");
+            fruitMap.put("aardappel", "U+1F954");
+
+            fruitMap.put("carrot", "U+1F955");
+            fruitMap.put("wortel", "U+1F955");
 
             fruitMap.put("avocado", "U+1F951");
 
             fruitMap.put("knoflook", "U+1F9C4");
             fruitMap.put("garlic", "U+1F9C4");
+
+            fruitMap.put("zout", "U+1F9C2");
+            fruitMap.put("salt", "U+1F9C2");
+
+            fruitMap.put("peper", "U+1F336");
+            fruitMap.put("pepper", "U+1F336");
+
+            fruitMap.put("pizza", "U+1F355");
+
+            fruitMap.put("kaas", "U+1F9C0");
+            fruitMap.put("cheese", "U+1F9C0");
+
+            fruitMap.put(" thee", "U+1F375");
+            fruitMap.put(" tea", "U+1F375");
+
+            fruitMap.put(" koffie", "U+2615");
+            fruitMap.put(" coffee", "U+2615");
         }
 }
