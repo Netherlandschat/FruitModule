c88e8414047d86b01f709972cd36f96ebd2d6a59
Daniël Vos <danielvos@outlook.com>
2021-06-28 21:56:00 +0200

    Add some fruits & veggies

---
+++ src/main/java/dev/array21/fruitmodule/FruitTypes.java
@@ -0,0 +1,40 @@
+package dev.array21.fruitmodule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitTypes extends FruitModule {
+        public static Map<String,String> fruitMap = new HashMap<>();
+        static {
+
+            fruitMap.put("banaan", "U+1F34C");
+            fruitMap.put("bananen", "U+1F34C");
+            fruitMap.put("banana", "U+1F34C");
+
+            fruitMap.put("appel", "U+1F34C");
+            fruitMap.put("apple", "U+1F34E");
+
+            fruitMap.put("peer", "U+1F350");
+            fruitMap.put("pear", "U+1F350");
+
+            fruitMap.put("aardbei", "U+1F353");
+            fruitMap.put("strawberr", "U+1F353");
+
+            fruitMap.put("mango", "U+1F96D");
+
+            fruitMap.put("tomaat", "U+1F345");
+            fruitMap.put("tomato", "U+1F345");
+
+            fruitMap.put("watermeloen", "U+1F349");
+            fruitMap.put("watermelon", "U+1F349");
+
+            fruitMap.put("broccoli", "U+1F966");
+
+            fruitMap.put("mais", "U+1F966");
+
+            fruitMap.put("avocado", "U+1F951");
+
+            fruitMap.put("knoflook", "U+1F9C4");
+            fruitMap.put("garlic", "U+1F9C4");
+        }
+}
--- src/main/java/dev/array21/fruitmodule/MessageEventListener.java
+++ src/main/java/dev/array21/fruitmodule/MessageEventListener.java
@@ -2,6 +2,8 @@ package dev.array21.fruitmodule;
 
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.util.Map;
+import java.util.Iterator;
 
 public class MessageEventListener extends ListenerAdapter {
 
@@ -11,9 +13,14 @@ public class MessageEventListener extends ListenerAdapter {
 			return;
 		}
 		
-		String message = event.getMessage().getContentDisplay();
-		if(message.toLowerCase().contains("avocado")) {
-			event.getMessage().addReaction("U+1F951").queue();
-		}
+		Iterator it = FruitTypes.fruitMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+
+            if(message.toLowerCase().contains(pair.getKey().toString())) {
+                event.getMessage().addReaction(pair.getValue().toString()).queue();
+            }
+        }
+
 	}
 }
